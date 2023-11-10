db67f8f87297e0cec6978624e96d43922799d222
gustavo <gustavo_x@live.com>
2023-11-10 10:51:55 -0300

    teste CI

--- weatherForecast/Controllers/WeatherForecastController.cs
+++ weatherForecast/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace weatherForecast.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
